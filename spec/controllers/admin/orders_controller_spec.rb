90430b549ee8632e33ee311c6ffb251048ed81a5
Anatina Trakowski <trakowski@taktsoft.com>
2017-04-18 22:15:46 +0200

    Fix redirect after resetting digitals
    for an order as admin user

--- spec/controllers/admin/orders_controller_spec.rb
+++ spec/controllers/admin/orders_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Spree::Admin::OrdersController do
       it 'should reset digitals for an order' do
         expect(order).to receive(:reset_digital_links!)
         spree_get :reset_digitals, id: order.number
-        expect(response).to redirect_to(spree.admin_order_path(order))
+        expect(response).to redirect_to(spree.edit_admin_order_url(order, only_path: true))
       end
     end
   end
